521998a25f6f783ecd98a1f741d4458c3d75ddcb
chaitanya448 <111422301+chaitanya448@users.noreply.github.com>
2022-11-03 14:04:40 +0530

    t=int(input()) for i in range(1,t+1):     for j in range (t,0,-1):         print(j,end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
-n=int(input())
-for i in range (n,0,-1):
-    for j in range (1,i+1):
-        print(j,end="")
+t=int(input())
+for i in range(1,t+1):
+    for j in range (t,0,-1):
+        print(j,end=" ")
     print()
